76855715f0b7048e036040f7eb00203b30e72f81
profremontpredst <mikhailfeskovavangard@gmail.com>
2025-12-20 21:18:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ document.getElementById("hellForm").addEventListener("submit", (e) => {
   console.log("HELL DOSSIER:", data);
 
   Telegram.WebApp.showAlert(
-    "Досье принято.\nСотрудник паспортного стола Ада свяжется с вами."
+    "Досье принято.\nСотрудник паспортного стола Ада приступил к обработке."
   );
 });
